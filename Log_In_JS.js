f5df6ba5d39eda22b71da0af81bd4e6114bda600
Atanelov <Atanelov@post.bgu.ac.il>
2024-02-20 15:33:21 +0200

    first commit

---
+++ Log_In_JS.js
@@ -0,0 +1,25 @@
+const login = document.querySelector('.login-container')
+const msg = document.querySelector('.msg')
+const username=document.querySelector('#username_input')
+const password=document.querySelector('#password_input')
+
+const onSubmit =(e) =>{
+    e.preventDefault()
+    console.log(username.value)
+    if(username.value==='' || password.value==='')
+    {
+        msg.innerHTML='please enter all fields !'
+        // setTimeout(()=>{
+        //     msg.innerHTML=''
+        //     msg.classList.remove('error',10000)
+        // })
+    }
+    else
+    {
+        username.value=''
+        password.value=''
+        msg.innerHTML=''
+    }
+
+}
+login.addEventListener('submit',onSubmit)
---
+++ Sign_Up.js
@@ -0,0 +1,47 @@
+const form = document.getElementById('form_')
+const firstName = document.querySelector('#firstName')
+const lastName = document.querySelector('#lastName')
+const userName = document.querySelector('#userName')
+const password = document.querySelector('#password')
+const universityMail = document.querySelector('#universityMail')
+const phoneNumber = document.querySelector('#phoneNumber')
+const address = document.querySelector('#address')
+const msg = document.querySelector('#msg')
+
+const onSubmit = (e)=> {
+    e.preventDefault()
+    let formTakin = true;
+    if (firstName.value === "" || lastName.value === "") {
+        msg.innerText += 'First and last name are required!\n'
+        formTakin = false;
+    }
+    if (userName.value === "") {
+        msg.innerText += 'Please choose a user name!\n'
+        formTakin = false;
+    }
+    if (password.value === ""|| password.value.length<8){
+        msg.innerText += 'Please choose a valid password with at least 8 characters!\n'
+        formTakin = false;
+    }
+    const companyEmailRegex = /@post.bgu\.ac.il$/;
+      if (universityMail.value === ""||!companyEmailRegex.test(universityMail.value)){
+        msg.innerText += 'Please enter your university mail!\n'
+        formTakin = false;
+    }
+      const phoneNumberRegex = /^\d+$/;
+      if (phoneNumber.value === ""||phoneNumber.value.length<10||phoneNumber.value.length>10
+          ||!phoneNumberRegex.test(phoneNumber.value)){
+        msg.innerText += 'Please a valid phone number with 10 digit numbers\n'
+        formTakin = false;
+    }
+      if (address.value === "") {
+          msg.innerText += 'Please enter your address!\n'
+          formTakin = false;
+      }
+        if (formTakin == false)
+            setTimeout(() => {
+                msg.innerText = "";
+        }, 4000);
+}
+form.addEventListener('submit', onSubmit);
+
